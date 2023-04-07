333939f24190226571b202179fe9cd706162b613
Harry Rendell <harryrendell9@gmail.com>
2023-04-07 11:58:14 +0100

    Moving SDSS to PS colour transformations colour_transform.py

--- analysis/funcs/preprocessing/colour_transform.py
+++ analysis/funcs/preprocessing/colour_transform.py
@@ -10,9 +10,25 @@ def transform_ztf_to_ps(df, obj, band):
     colors = pd.read_csv(cfg.USER.W_DIR+'data/computed/{}/colors_sdss.csv'.format(obj), usecols=[ID,'mean_gr','mean_ri']).set_index(ID)
     df = df.join(colors, how='left', on=ID).rename({'mag':'mag_orig'}, axis=1) #merge colors onto ztf df   
     if (band == 'r') | (band == 'g'):
-        df['mag'] = (df['mag_orig'] + df['clrcoeff']*df['mean_gr']).astype(cfg.COLLECTION.ZTF_dtypes.mag)
+        df['mag'] = (df['mag_orig'] + df['clrcoeff']*df['mean_gr']).astype(cfg.COLLECTION.ZTF.dtypes.mag)
     elif band == 'i':
-        df['mag'] = (df['mag_orig'] + df['clrcoeff']*df['mean_ri']).astype(cfg.COLLECTION.ZTF_dtypes.mag)
+        df['mag'] = (df['mag_orig'] + df['clrcoeff']*df['mean_ri']).astype(cfg.COLLECTION.ZTF.dtypes.mag)
     else:
         raise Exception('Unrecognised band: '+band)
+    return df[['mjd', 'mag', 'mag_orig', 'magerr']]
+
+def transform_sdss_to_ps(df, color='g-r', system='tonry'):
+    """
+    Add a column onto df with magnitudes transformed to the PS system.
+    There are few options of published transformations available. Here we use ones from Tonry 2012.
+    """
+    color_transf = pd.read_csv(cfg.USER.W_DIR+'analysis/transformations/transf_to_ps_{}.txt'.format(system), sep='\s+', index_col=0)
+    df = df.rename({'mag':'mag_orig'}, axis=1)
+    df['mag'] = 0
+    for band in 'griz':
+        a0, a1, a2, a3 = color_transf.loc[band].values
+        # Convert to PS mags
+        slidx = pd.IndexSlice[:, band]
+        x = df.loc[slidx, color]
+        df.loc[slidx, 'mag'] = df.loc[slidx, 'mag_orig'] + a0 + a1*x + a2*(x**2) + a3*(x**3)
     return df[['mjd', 'mag', 'mag_orig', 'magerr']]
