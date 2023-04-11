9332c17ac674f3aa88b590bc814a93ce714bd4bf
Harry Rendell <harryrendell9@gmail.com>
2023-04-11 17:06:42 +0100

    change refactor filter_data to work with new data format

--- analysis/funcs/preprocessing/parse.py
+++ analysis/funcs/preprocessing/parse.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 
 def intersection(*args):
+	"""
+	Reduce our data such that 
+	"""
 	surveys = args
 	for name, survey in zip(['sdss','ps  ','ztf '],surveys):
 		print(f'{name} before intersection: {len(survey):,}')
@@ -19,49 +22,36 @@ def intersection(*args):
 	print('---------------------------------')
 	return surveys
 
-def filter_data(survey_obj, bounds_generic, bounds_specific=None, bands=None, dropna=True, maxmagerr=np.inf):
-	key_mag = list(bounds_generic.keys())[0]
-	
-	if bands==None: #the sdss raw data has a different format to ztf and ps
-		print('num obs before: {:,}'.format(survey_obj[key_mag].notna().values.sum()))
-		boolean = np.stack([np.array(survey_obj[key].between(bound[0], bound[1])) for key, bound in bounds_generic.items()])
-		boolean = boolean.all(axis=0)
-		for key in bounds_generic.keys():
-			survey_obj.loc[:,key] = survey_obj[key].where(boolean.T)
-		
-		# Drop rows with no observations in any band
-		if dropna:
-			survey_obj = survey_obj.dropna(subset=[key_mag])
-		print('num obs after:  {:,}'.format(survey_obj[key_mag].notna().values.sum()))
+def filter_data(df, bounds, dropna=True, inplace=False):
+	"""
+	Remove data that lies outside ranges specified in bounds.
+	Note, using inplace=True is approx ~30% more memory efficient and prevents additional dataframes being stored.
+	Use inplace=False when testing bounds, but then switch to inplace=True once suitable bounds have been found. 
+	"""
 
-	else:
-# 		print('num obj before: {:,}'.format(survey_obj[[key_mag + '_' + b for b in bands]].notna().values.sum()))
-		print('num obj before: {:,}'.format(len(survey_obj.index)))
-
-		# Create set of boolean numpy arrays which are true if the key is within the bounds.
-# 		boolean_generic  = np.stack([np.array([survey_obj[key + '_' + b].between(bound[0], bound[1])) for key, bound in bounds_generic .items()])
-		boolean_generic  = np.array([np.array([survey_obj[key + '_' + b].between(bound[0], bound[1]) for b in bands]) for key, bound in bounds_generic.items()])
-	
-		if bounds_specific is not None:
-			boolean_specific = np.array([np.array(survey_obj[key].between(bound[0], bound[1])) for key, bound in bounds_specific.items()])
-			key2 = ['magerr_max_' + band for band in bands]
-			# Return .all() so that if either mag, meanerr or magerr_max is outside bounds, both values become NaN
-			boolean = np.concatenate([boolean_generic, boolean_specific[np.newaxis,:]]).all(axis=0).T
+	if not inplace:
+		df = df.copy()
+	# Create set of boolean numpy arrays which are true if the key is within the bounds.
+	for key, bound in bounds.items():
+		boolean = df[key].between(bound[0], bound[1])
+		if inplace:
+			df[key].where(boolean, np.nan, inplace=True)
 		else:
-			boolean = boolean_generic.all(axis=0).T
-			key2 = []
-			
-		for i, band in enumerate(bands):
-			key1 = [key + '_' + band for key in bounds_generic.keys()]
-# 			survey_obj.loc[:,key1+key2] = survey_obj.loc[:,key1+key2].where(boolean[:,i])
-			# Set the rows which do not satisfy the bands to NaN
-			survey_obj.loc[~boolean[:,i], key1+key2] = np.nan
-		# Drop rows with no observations in any band
-		if dropna:
-			survey_obj = survey_obj[survey_obj.loc[:,[key_mag + '_' + b for b in bands]].notna().any(axis=1)]
-		print('num obj after:  {:,}'.format(len(survey_obj.index)))
-		
-	return survey_obj
+			df[key] = df[key].where(boolean, np.nan, inplace=False)
+	
+	# Drop rows with no observations in any band
+	if dropna:
+		n = len(df.index)
+		if inplace:
+			df.dropna(axis=0, how='any', inplace=True)
+		else:
+			df = df.dropna(axis=0, how='any', inplace=False)
+		print('num obs before:  {:,}'.format(n))
+		print('num obs after:   {:,}'.format(len(df.index)))
+		print('num obs removed: {:,}'.format(n - len(df.index)))
+	
+	return df
+
 
 def compute_colors(survey):
     colors = pd.DataFrame()
