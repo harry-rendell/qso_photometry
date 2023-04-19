89bcd5d67d13a1d445d901ad497b6ac7c88a7a78
Harry Rendell <harryrendell9@gmail.com>
2023-04-19 18:22:14 +0100

    joining SDSS colours onto ZTF data should be an inner join to prevent creation of NaNs in transformed mags

--- module/preprocessing/colour_transform.py
+++ module/preprocessing/colour_transform.py
@@ -8,7 +8,7 @@ def transform_ztf_to_ps(df, obj, band):
     """
     ID = df.index.name
     colors = pd.read_csv(cfg.USER.W_DIR+'data/computed/{}/colors_sdss.csv'.format(obj), usecols=[ID,'mean_gr','mean_ri']).set_index(ID)
-    df = df.join(colors, how='left', on=ID).rename({'mag':'mag_orig'}, axis=1) #merge colors onto ztf df   
+    df = df.join(colors, how='inner', on=ID).rename({'mag':'mag_orig'}, axis=1) #merge colors onto ztf df   
     if (band == 'r') | (band == 'g'):
         df['mag'] = (df['mag_orig'] + df['clrcoeff']*df['mean_gr']).astype(cfg.COLLECTION.ZTF.dtypes.mag)
     elif band == 'i':
