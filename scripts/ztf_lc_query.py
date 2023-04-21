8d7c2b6faadf3f668d12c82035c9d575d178dce4
Harry Rendell <harryrendell9@gmail.com>
2023-04-21 15:10:55 +0100

    move ZTF parsing into ztf_lc_query, ie apply colour transformations and join uid onto ztf photometry.

--- scripts/ztf_lc_query.py
+++ scripts/ztf_lc_query.py
@@ -6,15 +6,17 @@ import sys
 from multiprocessing import Pool
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from module.config import cfg
+from module.preprocessing import colour_transform
 from urllib.error import HTTPError
 
 def request_and_save_photometry(oid_batch, suffix):
 	base_url = "https://irsa.ipac.caltech.edu/cgi-bin/ZTF/nph_light_curves?{}&FORMAT=csv&BAD_CATFLAGS_MASK=32768"
 	url = base_url.format(''.join(['&ID='+str(oid) for oid in oid_batch['oid']])[1:])
 	df = pd.read_csv(url, usecols = ['oid','mjd','mag','magerr','filtercode','clrcoeff','limitmag'], dtype=cfg.COLLECTION.ZTF.dtypes)
-	df = df.merge(oid_batch, on='oid', how='left', sort=False)
+	df = df.merge(oid_batch, on='oid', how='left', sort=False).sort_values(['uid','mjd'])
 	for band in 'gri':
-		df[df['filtercode']=='z'+band].to_csv(output_folder+'{}_band/lc_{:01d}.csv'.format(band, suffix), index=False, mode='a')
+		transformed_data = color_transform.transform_ztf_to_ps(df[df['filtercode']=='z'+band], OBJ, band)
+		transformed_data[SAVE_COLS].to_csv(output_folder+'{}_band/lc_{:01d}.csv'.format(band, suffix % 4), index=False, header=False, mode='a')
 
 def split_save(oid_batch, suffix, n_chunks):
 	# split into n chunks and save individually
@@ -22,7 +24,7 @@ def split_save(oid_batch, suffix, n_chunks):
 		request_and_save_photometry(oid_batch_halved, suffix)
 
 def obtain_ztf_lightcurves(suffix):
-	oids = np.array_split(pd.read_csv(ztf_oids_fname, usecols=['uid','oid'], dtype=np.uint64), cfg.USER.N_CORES)[suffix]
+	oids = np.array_split(pd.read_csv(ztf_oids_fname, usecols=['uid','oid'], dtype=np.uint64), n_workers)[suffix]
 	for i, oid_batch in enumerate(np.array_split(oids, n_requests)):
 		for n_chunks in [1,2,4,8]:
 			try:
@@ -40,22 +42,25 @@ def obtain_ztf_lightcurves(suffix):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--object", type=str, required=True, help ="qsos or calibStars")
-	parser.add_argument("--n_cores", type=int, help="Number of cores to use. If left blank, then this value is taken from N_CORES in the config file.")
-	parser.add_argument("--n_requests", type=int, default=1500, help="Total number of requests per core. We require this number s.t. len(ztf_oids.txt)/(n_request * n_cores) < 350. 350 is approx the max no. oids we can ask for in http request.")
+	parser.add_argument("--n_workers", type=int, required=True, help="Number of workers to use")
 	args = parser.parse_args()
-
 	print('args:',args)
-	if args.n_cores:
-		cfg.USER.N_CORES = args.n_cores
 
-	n_requests = args.n_requests
-	ztf_oids_fname  = cfg.USER.W_DIR + "python/pipeline/queries/ztf/{}/ztf_oids.csv".format(args.object)
-	output_folder = cfg.USER.W_DIR + "data/surveys/ztf/{}/dr6/".format(args.object)
+	OBJ = arg.object
+	SAVE_COLS = ['uid','oid','mjd','mag','magerr','mag_orig','clrcoeff','limitmag']
 
-	# clear previous saved lightcurves
+	total_requests = 6000
+	n_workers  = args.n_workers
+	n_requests = total_requests // n_workers
+
+	ztf_oids_fname  = cfg.USER.W_DIR + "python/pipeline/queries/ztf/{}/ztf_oids.csv".format(OBJ)
+	output_folder = cfg.USER.W_DIR + "data/surveys/ztf/{}/dr6/".format(OBJ)
+
+	# clear previous saved lightcurves and write header
 	for band in 'gri':
 		for i in range(4):
-			open(output_folder+'{}_band/lc_{:01d}.csv'.format(band, i), 'w').close()
+			with open(output_folder+'{}_band/lc_{:01d}.csv'.format(band, i), 'w') as file:
+				file.write(','.join(SAVE_COLS))
 
-	p = Pool(cfg.USER.N_CORES)
-	p.map(obtain_ztf_lightcurves,range(cfg.USER.N_CORES))
+	p = Pool(n_workers)
+	p.map(obtain_ztf_lightcurves,range(n_workers))
