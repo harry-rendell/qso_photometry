d1cdc151cebae92577a7ab9115113b8deacf6ede
Harry Rendell <harryrendell9@gmail.com>
2023-05-02 19:18:30 +0100

    New notebook for analysing specific objects

---
+++ analysis/weird_objects-NB.py
@@ -0,0 +1,115 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import os
+import sys
+sys.path.insert(0, os.path.join(os.getcwd(), ".."))
+from module.config import cfg
+from module.preprocessing import data_io, parse, lightcurve_statistics, pairwise
+from module.analysis.analysis import analysis
+from module.analysis.plotting_common import plot_series
+
+# +
+OBJ    = 'qsos'
+ID     = 'uid'
+BAND   = 'r'
+wdir   = cfg.USER.W_DIR
+nrows  = 100
+skiprows = None if nrows == None else nrows * 0
+SURVEY = 'ztf'
+
+kwargs = {'dtypes': cfg.PREPROC.lc_dtypes,
+          'nrows': nrows,
+          'skiprows': skiprows,
+          'basepath': cfg.USER.D_DIR + 'surveys/{}/{}/unclean/{}_band/fixed_lim_mag/'.format(SURVEY, OBJ, BAND), # we should make this path more general so it is consistent between surveys
+          'ID':ID}
+
+df = data_io.dispatch_reader(kwargs, multiproc=True)
+# -
+
+df['mag'].hist(bins=200)
+
+df['mag'].hist(bins=200)
+
+
+
+dr = analysis(ID, OBJ, BAND)
+
+dr.read_in()
+
+dr.df.index
+
+test = pairwise.groupby_dtdm_within(df)
+
+test
+
+mjd_mag_1 = df_ztf.loc[6].head()[['mjd','mag']].values
+mjd_mag_2 = df_sdss.loc[6][['mjd','mag']].values
+
+mjd_mag_1
+
+mjd_mag_2
+
+mjd_mag_1 - mjd_mag_2[:, np.newaxis, :]
+
+
+
+df_sdss.loc[6]
+
+axes
+
+axes = plot_series(df, uids)
+
+uids = [291224]
+axes = plot_series(df, uids)
+axes = plot_series(df, uids)
+axes.set_xlim([58580.26953125-20,58580.26953125+20])
+
+uids = 121363
+axes = plot_series(df, uids)
+axes = plot_series(df, uids)
+axes.set_xlim([58890,58900])
+
+# ---
+# ### Object 1
+# 134 ztf observations in one night.
+#
+
+lower = 58298.353
+upper = 58298.42
+dr.plot_series([471006], xlim=[lower,upper])
+dr.plot_series([471006])
+print('Time window for observations below: {:.2f} hours'.format((upper-lower)*24))
+
+dr.sdss_quick_look([471006])
+
+# position of object
+ra, dec = dr.coords.loc[471006].values
+print("https://skyserver.sdss.org/dr15/en/tools/chart/navi.aspx?ra={}&dec={}".format(ra, dec))
+
+# ---
+# ### Object 2
+
+coords = dr.coords.loc[[1,2]].values
+
+for ra, dec in coords:
+    print(ra, dec)
+
+dr.coords.loc[[1,2]]
+
+
