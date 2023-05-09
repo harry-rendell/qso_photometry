e092421bb48e335985b9365d275117bc1e2c8514
Harry Rendell <harryrendell9@gmail.com>
2023-05-09 11:45:30 +0100

    Add splitting of dataframe when computing dtdm

--- module/preprocessing/pairwise.py
+++ module/preprocessing/pairwise.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from ..config import cfg
+from .parse import split_into_non_overlapping_chunks
 
 def groupby_dtdm_between(df, args):
     for index, group in df.groupby(df.index.name):
@@ -51,26 +52,33 @@ def calculate_dtdm_within_surveys(group):
 def groupby_save_pairwise(df, kwargs):
     if not (('basepath' in kwargs) and ('fname' in kwargs)):
         raise Exception('Both basepath and fname must be provided')
-    if ('band' in kwargs) & ('band' in df.columns):
-        for b in kwargs['band']:
-            
-            output_dir = os.path.join(kwargs['basepath'], 'dtdm_'+b)
-            os.makedirs(output_dir, exist_ok=True)
-            
-            # If multiple bands are provided, iterate through them.
-            print(b,'band - processing:',kwargs['fname'])
-            s = df[df['band'] == b].groupby(df.index.name).apply(calculate_dtdm)
-            s = pd.DataFrame(s.values.tolist(), columns=s.columns, dtype='float32').astype({k:v for k,v in kwargs['dtypes'].items() if k in s.columns})
-            output_fname = kwargs['fname'].replace('lc','dtdm')
-            print(b,'band - saving:    ',output_fname)
-            s.to_csv(os.path.join(kwargs['basepath'], 'dtdm_'+b, output_fname), index=False)
+    
+    # splitting into 80 chunks and using 18 cores has a peak RAM of .. GB 
+    # splitting into 80 chunks and using 36 cores has a peak RAM of .. GB 
+    for chunk in split_into_non_overlapping_chunks(df, 80):
+        print('chunksize:',len(chunk))
+        if ('band' in kwargs) & ('band' in df.columns):
+            for b in kwargs['band']:
+                
+                output_dir = os.path.join(kwargs['basepath'], 'dt_dm_'+b)
+                os.makedirs(output_dir, exist_ok=True)
+
+                # If multiple bands are provided, iterate through them.
+                print(b,'band - processing:',kwargs['fname'])
+                s = chunk[chunk['band'] == b].groupby(chunk.index.name).apply(calculate_dtdm)
+                s = pd.DataFrame(s.values.tolist(), columns=s.columns, dtype='float32').astype({k:v for k,v in kwargs['dtypes'].items() if k in s.columns})
+                output_fpath = os.path.join(output_dir, kwargs['fname'].replace('lc','dtdm'))
+                
+                if not os.path.exists(output_fpath):
+                    with open(output_fpath, 'w') as file:
+                        file.write(','.join(s.columns) + '\n')
+
+                print(b,'band - saving:    ',kwargs['fname'].replace('lc','dtdm'))
+                s.to_csv(output_fpath, index=False, header=False, mode='a')
+                del s
 
     else:
-        output_dir = os.path.join(kwargs['basepath'], 'dtdm_')
-        os.makedirs(output_dir, exist_ok=True)
-        s = df[mask].groupby(df.index.name).apply(calculate_dtdm)
-        s = pd.DataFrame(s.values.tolist(), index=s.index, dtype='float32')
-        s.to_csv(os.path.join(kwargs['basepath'], 'dtdm_'+b, kwargs['fname'].replace('lc','dtdm')))
+        raise Exception('error!!!')
 
 def calculate_dtdm(group):
     mjd_mag = group[['mjd','mag']].values
