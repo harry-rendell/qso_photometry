82a099a5fedcb4606d4429fdd5a83eb18995139c
Harry Rendell <harryrendell9@gmail.com>
2023-05-09 16:08:52 +0100

    Finalising the sql queries

--- pipeline/queries/ps/PS_neighbour_search.sql
+++ pipeline/queries/ps/PS_neighbour_search.sql
@@ -1,15 +1,33 @@
 -- This query searches for Pan-STARRS object IDs which correspond to the DR14 quasars, and saves them as objID_ps
 -- Note that we use fGetNearbyObjEq, which may return multiple objID_ps for a given uid. This is because the PS stacking is not perfect.
+
+-- qsos
 select
 
 q.uid as uid, o.objID as objID_ps, 
 q.ra, q.dec,
 nb.distance as sep
 
-into mydb.dr14q_ps_neighbours
-from mydb.dr14q_uid_coords q
+into mydb.ps_neighbours_qsos
+from mydb.qsos_subsample_coords q
 
 cross apply dbo.fGetNearbyObjEq(q.ra, q.dec, 1.0/60) as nb
 join StackObjectThin o on o.objid = nb.objid where o.primaryDetection = 1
 
 order by uid ASC
+
+-- calibStars
+
+select
+
+q.uid_s as uid_s, o.objID as objID_ps, 
+q.ra, q.dec,
+nb.distance as sep
+
+into mydb.ps_neighbours_calibStars
+from mydb.calibStars_subsample_coords q
+
+cross apply dbo.fGetNearbyObjEq(q.ra, q.dec, 1.0/60) as nb
+join StackObjectThin o on o.objid = nb.objid where o.primaryDetection = 1
+
+order by uid_s ASC
