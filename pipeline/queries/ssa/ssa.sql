7e8fe0d6b933d01b645e610b70f1b46f33f8d512
Harry Rendell <harryrendell9@gmail.com>
2023-05-16 15:45:05 +0100

    More comments on ssa.sql

--- pipeline/queries/ssa/ssa.sql
+++ pipeline/queries/ssa/ssa.sql
@@ -13,8 +13,9 @@ from Plate
 ORDER BY surveyID, plateID
 
 -- 2. Upload chunks of coordinates to ssa.roe.ac.uk/xmatch.html
---      Pairing radius = 1" (note: entry field is in arcmin)
+--      Pairing radius = 3". Filter down to 1.5" ish later. (note: entry field is in arcmin, so use 0.05) 
 --      Paring option = All nearby (if we choose Nearest then we only get 1 filterband per object)
 --      Data Format = ASCII
 --      Select: objID,ssaField,surveyID,plateID,smag
 --      From: Detection (use Detection instead of source so that we have a breakdown of filterbands)
+-- note: do not submit queries all at once, and make sure correct number of rows have been uploaded.
