cf86f8680636bcd6f31fe1ee2764e16ab8b9c763
Harry Rendell <harryrendell9@gmail.com>
2023-05-23 14:18:51 +0100

    Change print statements in parse.intersection

--- module/preprocessing/parse.py
+++ module/preprocessing/parse.py
@@ -2,57 +2,57 @@ import pandas as pd
 import numpy as np
 
 def intersection(*args):
-	"""
-	Find intersection of objects for which we have data in surveys provided in args
-	TODO:
-		We don't need to pass in all data, instead just pass in the analysis class and fetch the unique uids from there.
-	"""
-	surveys = args
-	for name, survey in zip(['sdss','ps  ','ztf '],surveys):
-		print(f'{name} before intersection: {len(survey):,}')
+    """
+    Find intersection of objects for which we have data in surveys provided in args
+    TODO:
+        We don't need to pass in all data, instead just pass in the analysis class and fetch the unique uids from there.
+    """
+    surveys = args
+    for name, survey in enumerate(surveys):
+        print(f'{name} before intersection: {len(survey):,}')
 
-	indicies	 = [survey.index.get_level_values(0) for survey in surveys]
-	indicies_set = [set(index) for index in indicies]
+    indicies     = [survey.index for survey in surveys]
+    indicies_set = [set(index) for index in indicies]
 
-	# Find the set of uid_s that exist in all three surveys
-	intersection = set.intersection(*indicies_set)
+    # Find the set of uid_s that exist in all three surveys
+    intersection = set.intersection(*indicies_set)
 
-	surveys = [survey[index.isin(intersection)] for survey, index in zip(surveys, indicies)]
-	print('---------------------------------')
-	for name, survey in zip(['sdss','ps  ','ztf '],surveys):
-		print(f'{name} after  intersection: {len(survey):,}')
-	print('---------------------------------')
-	return surveys
+    surveys = [survey[index.isin(intersection)] for survey, index in zip(surveys, indicies)]
+    print('---------------------------------')
+    for name, survey in enumerate(surveys):
+        print(f'{name} after  intersection: {len(survey):,}')
+    print('---------------------------------')
+    return surveys
 
 def filter_data(df, bounds={}, dropna=True, valid_uids=None):
-	"""
-	Remove data that lies outside ranges specified in bounds.
-	Note, using inplace=True is approx ~30% more memory efficient and prevents additional dataframes being stored.
-	Use inplace=False when testing bounds, but then switch to inplace=True once suitable bounds have been found. 
-	Note, the bounds are INCLUSIVE.
-	"""
-	# Restrict our dataframe rows with indices contained in valid_uids, if provided.
-	df = df.copy()
-	n = len(df.index)
-	if valid_uids is not None:
-		mask = df.index.isin(valid_uids.index)
-		df = df[mask]
-		print('No. rows removed that are not in valid_uids: {:,}\n'.format( (~mask).sum() ))
-	# Create set of boolean numpy arrays which are true if the key is within the bounds.
-	for key, bound in bounds.items():
-		boolean = df[key].between(bound[0], bound[1])
-		print('Enforcing {:.2f} <= {} <= {:.2f}'.format(bound[0],key,bound[1]).ljust(50,' ') + 'No. points outside bounds: {:,}'.format((~boolean).sum()))
-		df[key] = df[key].where(boolean, np.nan, inplace=False)
-	
-	# Drop rows with no observations in any band
-	if dropna:
-		df = df.dropna(axis=0, how='any', inplace=False)
-		print('-'*85)
-		print('No. rows before:  {:,}'.format(n))
-		print('No. rows after:   {:,}'.format(len(df.index)))
-		print('No. rows removed: {:,} ({:.2f}%)'.format(n - len(df.index), (1-len(df.index)/n)*100))
-	
-	return df
+    """
+    Remove data that lies outside ranges specified in bounds.
+    Note, using inplace=True is approx ~30% more memory efficient and prevents additional dataframes being stored.
+    Use inplace=False when testing bounds, but then switch to inplace=True once suitable bounds have been found. 
+    Note, the bounds are INCLUSIVE.
+    """
+    # Restrict our dataframe rows with indices contained in valid_uids, if provided.
+    df = df.copy()
+    n = len(df.index)
+    if valid_uids is not None:
+        mask = df.index.isin(valid_uids.index)
+        df = df[mask]
+        print('No. rows removed that are not in valid_uids: {:,}\n'.format( (~mask).sum() ))
+    # Create set of boolean numpy arrays which are true if the key is within the bounds.
+    for key, bound in bounds.items():
+        boolean = df[key].between(bound[0], bound[1])
+        print('Enforcing {:.2f} <= {} <= {:.2f}'.format(bound[0],key,bound[1]).ljust(50,' ') + 'No. points outside bounds: {:,}'.format((~boolean).sum()))
+        df[key] = df[key].where(boolean, np.nan, inplace=False)
+    
+    # Drop rows with no observations in any band
+    if dropna:
+        df = df.dropna(axis=0, how='any', inplace=False)
+        print('-'*85)
+        print('No. rows before:  {:,}'.format(n))
+        print('No. rows after:   {:,}'.format(len(df.index)))
+        print('No. rows removed: {:,} ({:.2f}%)'.format(n - len(df.index), (1-len(df.index)/n)*100))
+    
+    return df
 
 def compute_colors(survey):
     colors = pd.DataFrame()
