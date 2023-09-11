4e4ae433b465e330388fd93c969d070ddf6fb7db
Harry Rendell <harryrendell9@gmail.com>
2023-09-11 11:19:43 +0100

    Refactor create conda env script

--- create_env_and_pkgs.sh
+++ create_env_and_pkgs.sh
@@ -1,14 +1,8 @@
 source ~/.bashrc_old
+yes | conda update -n base -c defaults conda
 yes | conda create -n astro python=3.9.16
 conda activate astro
-yes | conda update -n base -c defaults conda
 yes | conda install numpy pandas matplotlib scipy seaborn jupyter
-yes | conda install -c conda-forge jupyterlab
-yes | conda install -c conda-forge jupytext
-yes | conda install -c conda-forge easydict
-yes | conda install -c conda-forge astropy
-yes | conda install -c conda-forge celerite
-yes | conda install -c conda-forge jupyterlab-git
-yes | conda install -c conda-forge jupyter_nbextensions_configurator
+yes | conda install -c conda-forge jupyterlab jupytext easydict astropy celerite jupyterlab-git jupyter_nbextensions_configurator venn scikit-learn autograd
+yes | conda install -c astropy emcee
 yes | conda update -n base -c defaults conda
-
