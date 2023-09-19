1a1c087565929ced15b441012119699ae7a0feeb
Harry Rendell <harryrendell9@gmail.com>
2023-09-19 15:20:10 +0100

    Add VAC to assets

--- module/assets.py
+++ module/assets.py
@@ -1,7 +1,9 @@
 from .config import cfg
 import pandas as pd
+import os
+from .preprocessing import parse
 
-def load_grouped(obj, band=None, return_dict=True):
+def load_grouped(obj, bands=None, return_dict=True):
     """
     Load grouped data for a given object and band.
     If return_dict = True, then return a dictionary of DataFrames.
@@ -9,28 +11,28 @@ def load_grouped(obj, band=None, return_dict=True):
         sdss, ps, ztf, ssa, tot
     """
     ID = 'uid' if obj == 'qsos' else 'uid_s'
-    if band is not None:
-        sdss = pd.read_csv(cfg.D_DIR + f'surveys/sdss/{obj}/clean/{band}_band/grouped.csv', index_col=ID)
-        ps   = pd.read_csv(cfg.D_DIR + f'surveys/ps/{obj}/clean/{band}_band/grouped.csv', index_col=ID)
-        ztf  = pd.read_csv(cfg.D_DIR + f'surveys/ztf/{obj}/clean/{band}_band/grouped.csv', index_col=ID)
-        ssa  = pd.read_csv(cfg.D_DIR + f'surveys/supercosmos/{obj}/clean/{band}_band/grouped.csv', index_col=ID)
+    if len(bands) == 1:
+        sdss = pd.read_csv(cfg.D_DIR + f'surveys/sdss/{obj}/clean/{bands}_band/grouped.csv', index_col=ID)
+        ps   = pd.read_csv(cfg.D_DIR + f'surveys/ps/{obj}/clean/{bands}_band/grouped.csv', index_col=ID)
+        ztf  = pd.read_csv(cfg.D_DIR + f'surveys/ztf/{obj}/clean/{bands}_band/grouped.csv', index_col=ID)
+        ssa  = pd.read_csv(cfg.D_DIR + f'surveys/supercosmos/{obj}/clean/{bands}_band/grouped.csv', index_col=ID)
     else:
-        sdss = {band:pd.read_csv(cfg.D_DIR + f'surveys/sdss/{obj}/clean/{b}_band/grouped.csv'       , index_col=ID) for b in 'gri'}
-        ps   = {band:pd.read_csv(cfg.D_DIR + f'surveys/ps/{obj}/clean/{b}_band/grouped.csv'         , index_col=ID) for b in 'gri'}
-        ztf  = {band:pd.read_csv(cfg.D_DIR + f'surveys/ztf/{obj}/clean/{b}_band/grouped.csv'        , index_col=ID) for b in 'gri'}
-        ssa  = {band:pd.read_csv(cfg.D_DIR + f'surveys/supercosmos/{obj}/clean/{b}_band/grouped.csv', index_col=ID) for b in 'gri'}
+        sdss = {b:pd.read_csv(cfg.D_DIR + f'surveys/sdss/{obj}/clean/{b}_band/grouped.csv'       , index_col=ID) for b in bands}
+        ps   = {b:pd.read_csv(cfg.D_DIR + f'surveys/ps/{obj}/clean/{b}_band/grouped.csv'         , index_col=ID) for b in bands}
+        ztf  = {b:pd.read_csv(cfg.D_DIR + f'surveys/ztf/{obj}/clean/{b}_band/grouped.csv'        , index_col=ID) for b in bands}
+        ssa  = {b:pd.read_csv(cfg.D_DIR + f'surveys/supercosmos/{obj}/clean/{b}_band/grouped.csv', index_col=ID) for b in bands}
     
     if return_dict:
         return {'sdss':sdss, 'ps':ps, 'ztf':ztf, 'ssa':ssa}
     else:
         return sdss, ps, ztf, ssa
     
-def load_grouped_tot(obj, band=None):
+def load_grouped_tot(obj, bands=None):
     ID = 'uid' if obj == 'qsos' else 'uid_s'
-    if band is not None:
-        tot  = pd.read_csv(cfg.D_DIR + f'merged/{obj}/clean/grouped_{band}.csv', index_col=ID)
+    if len(bands) == 1:
+        tot  = pd.read_csv(cfg.D_DIR + f'merged/{obj}/clean/grouped_{bands}.csv', index_col=ID)
     else:
-        tot = {band:pd.read_csv(cfg.D_DIR + f'merged/{obj}/clean/grouped_{b}.csv', index_col=ID) for b in 'gri'}
+        tot = {bands:pd.read_csv(cfg.D_DIR + f'merged/{obj}/clean/grouped_{b}.csv', index_col=ID) for b in bands}
 
     return tot
 
@@ -44,3 +46,56 @@ def load_coords(obj):
 
 def load_redshifts():
     return pd.read_csv(cfg.D_DIR + f'catalogues/qsos/dr14q/dr14q_redshift.csv', index_col='uid').squeeze()
+
+def load_vac(obj, catalogue_name='dr16q_vac'):
+    """
+    Load value-added catalogues for our quasar sample.
+    Options of:
+        dr12_vac
+            Kozlowski 2016
+            https://arxiv.org/abs/1609.09489
+        dr14_vac
+            Rakshit 2020
+            https://arxiv.org/abs/1910.10395
+        dr16q_vac : Preferred
+            Shen 2022
+            https://arxiv.org/abs/2209.03987
+    """
+    ID = 'uid'
+    if obj == 'calibStars':
+        raise Exception('Stars have no value-added catalogues')
+    
+    if catalogue_name == 'dr12_vac':
+        # cols = z, Mi, L5100, L5100_err, L3000, L3000_err, L1350, L1350_err, MBH_MgII, MBH_CIV, Lbol, Lbol_err, nEdd, sdss_name, ra, dec, uid
+        fpath = 'catalogues/qsos/dr12q/SDSS_DR12Q_BH_matched.csv'
+        prop_range_all = {'Mi':(-30,-20),
+                          'mag_mean':(15,23.5),
+                          'mag_std':(0,1),
+                          'redshift':(0,5),
+                          'Lbol':(44,48),
+                          'nEdd':(-3,0.5)}
+
+    elif catalogue_name == 'dr14_vac':
+        # cols = ra, dec, uid, sdssID, plate, mjd, fiberID, z, pl_slope, pl_slope_err, EW_MgII_NA, EW_MgII_NA_ERR, FWHM_MgII_NA, FWHM_MgII_NA_ERR, FWHM_MgII_BR, FWHM_MgII_BR_ERR, EW_MgII_BR, EW_MgII_BR_ERR, MBH_CIV, MBH_CIV_ERR, MBH, MBH_ERR, Lbol
+        fpath = 'catalogues/qsos/dr14q/dr14q_spec_prop_matched.csv'
+        prop_range_all = {'mag_mean':(15,23.5),
+                          'mag_std':(0,1),
+                          'redshift':(0,5),
+                          'Lbol':(44,48)}
+
+    elif catalogue_name == 'dr16q_vac':
+        # cols = ra, dec, redshift_vac, Lbol, Lbol_err, MBH_HB, MBH_HB_err, MBH_MgII, MBH_MgII_err, MBH_CIV, MBH_CIV_err, MBH, MBH_err, nEdd, nEdd_err
+        fpath = 'catalogues/qsos/dr16q/dr16q_vac_shen_matched.csv'
+        prop_range_all = cfg.PREPROC.VAC_BOUNDS
+    else:
+        raise Exception('Unrecognised value-added catalogue')
+
+    vac = pd.read_csv(os.path.join(cfg.D_DIR,fpath), index_col=ID)
+    vac = vac.rename(columns={'z':'redshift_vac'});
+    if catalogue_name == 'dr16q_vac':
+        # Note, in dr16q, bad nEdd entries are set to 0 (exactly) so we can remove those.
+        vac['nEdd'] = vac['nEdd'].where((vac['nEdd']!=0).values)
+    
+    vac = parse.filter_data(vac, prop_range_all, dropna=False)
+
+    return vac
