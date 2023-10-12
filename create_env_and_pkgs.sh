6b506168796a3871768dcd264d87a231a4d9740d
Harry Rendell <harryrendell9@gmail.com>
2023-10-12 14:16:32 +0100

    add dnest4, emcee and corner packages

--- create_env_and_pkgs.sh
+++ create_env_and_pkgs.sh
@@ -3,6 +3,6 @@ yes | conda update -n base -c defaults conda
 yes | conda create -n astro python=3.9.16
 conda activate astro
 yes | conda install numpy pandas matplotlib scipy seaborn jupyter
-yes | conda install -c conda-forge jupyterlab jupytext easydict astropy celerite jupyterlab-git jupyter_nbextensions_configurator venn scikit-learn autograd
-yes | conda install -c astropy emcee
+yes | conda install -c conda-forge jupyterlab jupytext easydict astropy celerite jupyterlab-git jupyter_nbextensions_configurator venn scikit-learn autograd emcee dnest4
+yes | conda install -c astropy corner
 yes | conda update -n base -c defaults conda
