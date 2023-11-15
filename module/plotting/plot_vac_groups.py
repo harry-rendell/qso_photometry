a30d430e22562e9a5e410772ce3f4d211991966c
Harry Rendell <harryrendell9@gmail.com>
2023-11-15 14:48:07 +0000

    New function to plot lbol/lambda grouping

--- module/plotting/plot_vac_groups.py
+++ module/plotting/plot_vac_groups.py
@@ -4,6 +4,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from module.config import cfg
 import matplotlib.pyplot as plt
 from module.preprocessing.binning import calculate_groups
+import numpy as np
+import seaborn as sns
 
 def plot_groups(x, bounds, plot=False, hist_kwargs={}, ax_kwargs={}):
     """
@@ -22,5 +24,39 @@ def plot_groups(x, bounds, plot=False, hist_kwargs={}, ax_kwargs={}):
         # a gap between text
         ax.text(x=value, y=1.01, s=r'${}\sigma$'.format(z), horizontalalignment='center', transform=ax.get_xaxis_transform(), fontsize='small')
     ax.set(xlim=[bounds_values[0],bounds_values[-1]], **ax_kwargs)
+
+    return fig
+
+def plot_groups_lambda_lbol(df, mask_dict, n=15, l_low=1000, l_high=5000, L_low=45.2, L_high=47.2):
+    # Create matplotlib polygon with vertices at the bin edges of Lbol_edges and lambda_edges
+    from matplotlib.collections import PatchCollection
+    from matplotlib.patches import Rectangle
+
+    Lbol_edges   = np.linspace(L_low, L_high, n)
+    lambda_edges = np.linspace(l_low, l_high, n)
+
+    # # create a series of 2d bins from the edges
+    # Lbol_bins = pd.cut(sigs['Lbol'], Lbol_edges, labels=False)
+    # lambda_bins = pd.cut(sigs['wavelength'], lambda_edges, labels=False)
+
+    # # masks = [(Lbol_bins == L).values & (lambda_bins == l).values for l,L in itertools.product(range(n-1), range(n-1))]
+    # masks_full = {(l,L):(Lbol_bins == L).values & (lambda_bins == l).values for l,L in itertools.product(range(n-1), range(n-1))}
+    # masks = {key:value for key,value in masks_full.items() if (value.sum() > 250) and (key[0] % 3 == 0) and (key[1] % 3 == 0)}
+
+    fig, ax = plt.subplots(1,1, figsize=(10,10))
+    binrange = np.array([[l_low-100, l_high+100], [L_low-0.5, L_high+0.5]])
+    sns.histplot(data=df.reset_index(), x='wavelength',y='Lbol', bins=100, cmap='Spectral_r', binrange=binrange, ax=ax)
+
+    vertices = [[lambda_edges[i], Lbol_edges[j]] for i,j in mask_dict.keys()]
+    squares = [Rectangle(vertex, width=(l_high-l_low)/(n-1), height=2/(n-1)) for vertex in vertices]
+    # add text to each square, showing the i, j indices
+
+    p = PatchCollection(squares, alpha=1, lw=2, ec='k', fc='none')
+    ax.add_collection(p)
+    ax.set(xlim=binrange[0], ylim=binrange[1], xlabel='wavelength', ylabel='Lbol')
+
+
+    for i, j in mask_dict.keys():
+        ax.text(lambda_edges[i]+2000/(n-1), Lbol_edges[j]+1/(n-1), s=f'({i},{j})', ha='center', va='center', fontsize=8)
 
     return fig
