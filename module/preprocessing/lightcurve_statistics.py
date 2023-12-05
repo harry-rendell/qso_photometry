Fix incorrect weighting of SF

module/preprocessing/lightcurve_statistics.py
@@ -65,12 +65,13 @@ def calculate_sf_per_qso(group, kwargs):
     for i in range(n_points):
         mask = (mjd_edges[i] < dt) & (dt <= mjd_edges[i+1])
         if mask.sum()==0:
-            a[i,:] = np.nan
+            a[i,1:] = np.nan
         else:
-            a[i,0] = np.average(dm[mask]**2 - de[mask]**2, weights=de[mask]**-2) # SF2_cw
-            a[i,1] = np.average(dm[mask]**2, weights=de[mask]**-2) # SF2_w
-            a[i,2] = dm[mask].var() # dm_var
-        a[i,3] = int(mask.sum()) # n
+            a[i,0] = int(mask.sum())
+            weights = 0.5*de[mask]**-4
+            a[i,1] = np.average(dm[mask]**2 - de[mask]**2, weights=weights) # SF2_cw
+            a[i,2] = np.average(dm[mask]**2, weights=weights) # SF2_w
+            a[i,3] = 1/weights.sum() # SF_err
 
     return {f'{key}_{mjd_edges[i]}_{mjd_edges[i+1]}':a[i,j] for i in range(n_points) for j, key in enumerate(features)}
 