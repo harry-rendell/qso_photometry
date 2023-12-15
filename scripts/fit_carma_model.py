47935cd7065b082a63a52402727f3f87439236fd
Harry Rendell <harryrendell9@gmail.com>
2023-12-15 15:14:48 +0000

    Comments + include n_obs threshold

--- scripts/fit_carma_model.py
+++ scripts/fit_carma_model.py
@@ -7,16 +7,23 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from module.config import cfg
 from module.preprocessing import data_io
 from module.modelling import carma
-from module.assets import load_grouped_tot
+from module.assets import load_n_tot
 from functools import partial
+
+"""
+This script fits a CARMA model to the photometric data for a given object and band.
+It is analagous to the fit_drw_mcmc.py script, but does not do an MCMC fit.
+Can be used to fit either a DRW or DHO model.
+"""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--object",  type=str, required=True, help ="qsos or calibStars")
     parser.add_argument("--band",    type=str, required=True, help="One or more filterbands for analysis")
     parser.add_argument("--n_cores", type=int, required=True, help="Number of cores to use. If left blank, then this value is taken from N_CORES in the config file.")
+    parser.add_argument("--frame",   type=str, required=True, help=("OBS or REST to specify rest frame or observer frame time. \n"
+                                                                    "Defaults to rest frame for Quasars and observer time for Stars.\n"))
     parser.add_argument("--n_rows",  type=int, help="Number of rows to read in from the photometric data")
-    parser.add_argument("--dry_run", action='store_true', help="Whether to do a dry run (i.e. don't save the output)")
     parser.add_argument("--survey",   type=str, help="name of survey to restrict data to. If left blank, then all surveys are used.")
     parser.add_argument("--model",  type=str, required=True, help ="Model to fit to the data. Options are drw or dho.")
     parser.add_argument("--nobs_min",type=int, help="Minimum number of observations required to fit a model.")
@@ -29,11 +36,15 @@ if __name__ == "__main__":
     OBJ = args.object
     if OBJ == 'qsos':
         ID = 'uid'
-        mjd_key = 'mjd_rf'
-        # mjd_key = 'mjd'
     else:
         ID = 'uid_s'
+    
+    if args.frame.lower() == 'obs':
         mjd_key = 'mjd'
+    elif args.frame.lower() == 'rest':
+        mjd_key = 'mjd_rf'
+    else:
+        raise ValueError('frame must be either OBS or REST')
 
     if args.best_phot:
         phot_str = 'best_phot'
@@ -53,18 +64,29 @@ if __name__ == "__main__":
               'mjd_key':mjd_key}
 
     if args.survey:
-        kwargs['sid'] = cfg.PREPROC.SURVEY_IDS[args.survey]
+        kwargs['sid'] = [cfg.PREPROC.SURVEY_IDS[s] for s in args.survey.split()]
     else:
         args.survey = 'all'
 
-    for band in args.band:
-        # add these back into the kwargs dictionary
-        if args.nobs_min:
-            n_tot = load_grouped_tot(OBJ, band, usecols=['n_tot']).squeeze()
-            uid_subset = n_tot[(n_tot > args.nobs_min).values].index
-            kwargs['subset'] = uid_subset
+    if args.nobs_min:
+        # We select a subset of quasars which have a minimum of args.nobs_min observations in each of the bands 
+        #   specified in args.band
+        # ignore ztf when using n_tot as a threshold if args.survey is not ztf
+        n_tot = load_n_tot(OBJ)
+        if args.survey != 'ztf':
+            survey_str = args.survey.replace('ztf','').split()
         else:
-            args.nobs_min = 0
+            survey_str = args.survey.split()
+
+        # mask = np.all([(n_tot[[f'n_{s}_{b}' for s in survey_str]].sum(axis=1) > args.nobs_min) for b in args.band], axis=0)
+        # kwargs['subset'] = n_tot[mask].index
+    else:
+        args.nobs_min = 0
+
+    for band in args.band:
+        if args.nobs_min > 0:
+            mask = (n_tot[[f'n_{s}_{band}' for s in survey_str]].sum(axis=1) > args.nobs_min)
+            kwargs['subset'] = n_tot[mask].index
 
         kwargs['band'] = band
         kwargs['basepath'] = cfg.D_DIR + f'merged/{OBJ}/{phot_str}/'
@@ -80,6 +102,7 @@ if __name__ == "__main__":
             raise ValueError('Invalid model selected. Options are drw or dho.')
 
         results = data_io.dispatch_function(f, max_processes=cfg.USER.N_CORES, concat_output=True, **kwargs)
-        results.to_csv(cfg.D_DIR + f'computed/{OBJ}/features/{args.model}_fits_{band}_{args.survey}_{args.nobs_min}_{phot_str}_rest_frame.csv')
-        # results.to_csv(cfg.D_DIR + f'computed/{OBJ}/features/{args.model}_fits_{band}_{args.survey}_{args.nobs_min}_{phot_str}.csv')
+        output_dir = cfg.D_DIR + f'computed/{OBJ}/{args.model}_fits/{args.frame}/'
+        os.makedirs(output_dir, exist_ok=True)
+        results.to_csv(output_dir + f"{band}_{args.survey.replace(' ','_')}_{args.nobs_min}_{phot_str}.csv")
         print('Elapsed:',time.strftime("%Hh %Mm %Ss",time.gmtime(time.time()-start)))
