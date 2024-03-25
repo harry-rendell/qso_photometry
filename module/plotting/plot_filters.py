6a37f4630f86b17d980f73e95dfd4537fb5994be
Harry Rendell <harryrendell9@gmail.com>
2024-03-25 11:58:44 +0000

    improving figure style

--- module/plotting/plot_filters.py
+++ module/plotting/plot_filters.py
@@ -23,26 +23,26 @@ def plot_filters(**kwargs):
 	colors = [cfg.FIG.COLORS.BANDS[c] for c in 'gri']
 
 	# Getting everything into the same units
-	ztf_g.loc[:, 'g'] /= 100
-	ztf_r.loc[:, 'r'] /= 100
-	ztf_i.loc[:, 'i'] /= 100
+	sdss.loc[:, list('ugriz')] *= 100
+	ps.loc[:, list('grizy')] *= 100
 	ztf_g.loc[:,'lambda'] *= 10
 	ztf_r.loc[:,'lambda'] *= 10
 	ztf_i.loc[:,'lambda'] *= 10
 	ps.loc[:,'lambda'] *= 10
 
 	fig, ax = plt.subplots(1,1, figsize=(10,5))
-	ztf_g.plot(x='lambda', y='g', ax=ax, ls='-', ms=10, label='ztf_g', color = cfg.FIG.COLORS.BANDS['g'])
-	ztf_r.plot(x='lambda', y='r', ax=ax, ls='-', ms=10, label='ztf_r', color = cfg.FIG.COLORS.BANDS['r'])
-	ztf_i.plot(x='lambda', y='i', ax=ax, ls='-', ms=10, label='ztf_i', color = cfg.FIG.COLORS.BANDS['i'])
+	ztf_g.plot(x='lambda', y='g', ax=ax, ls='-', lw=1, ms=10, label=r''+cfg.SURVEY_LABELS['ztf'] + ' $g$', color = cfg.FIG.COLORS.BANDS['g'])
+	ztf_r.plot(x='lambda', y='r', ax=ax, ls='-', lw=1, ms=10, label=r''+cfg.SURVEY_LABELS['ztf'] + ' $r$', color = cfg.FIG.COLORS.BANDS['r'])
+	ztf_i.plot(x='lambda', y='i', ax=ax, ls='-', lw=1, ms=10, label=r''+cfg.SURVEY_LABELS['ztf'] + ' $i$', color = cfg.FIG.COLORS.BANDS['i'])
 
-	sdss.plot(x='lambda', y=['g','r','i'], ax=ax, ls='--' , ms=10, label=['sdss_'+b for b in 'gri'], color = colors)
-	ps  .plot(x='lambda', y=['g','r','i'], ax=ax, ls='-.', ms=10, label=['ps_' + b for b in 'gri'], color = colors)
+	ps  .plot(x='lambda', y=['g','r','i'], ax=ax, ls=(0, (3, 1, 1, 1)), ms=1, label=[r''+cfg.SURVEY_LABELS['ps'  ]+' $'+ b + '$' for b in 'gri'], color = colors)
+	sdss.plot(x='lambda', y=['g','r','i'], ax=ax, ls='--', dashes=(5,1), ms=1, label=[r''+cfg.SURVEY_LABELS['sdss']+' $'+ b + '$' for b in 'gri'], color = colors)
 # 	sdss.plot(x='lambda', y=['g','r','i','z'], ax=ax, ls='-' , ms=10, label=['sdss_'+b for b in 'griz'], color = list('grbk'))
 # 	ps  .plot(x='lambda', y=['g','r','i','z'], ax=ax, ls='-.', ms=10, label=['ps_' + b for b in 'griz'], color = list('grbk'))
 
-	ax.set(xlabel='wavelength (Å)', ylabel='transmission', **kwargs);
+	ax.set(xlabel='wavelength (Å)', ylabel='transmission (%)', **kwargs);
 	ax.get_legend().remove()
+	ax.legend(ncol=3, loc='upper right', bbox_to_anchor=(1,1.22))
 	ztf = [ztf_g, ztf_r, ztf_i]
 	
 	return fig, ax, sdss, ps, ztf
